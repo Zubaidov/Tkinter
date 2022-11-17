a7f785d32876fa203656e9d394b9fcf0127dabff
Zubaidov <shohruz.zubaidov@gmail.com>
2022-11-17 23:15:36 +0400

    Lesson 3 All about Button

--- Lesson3_Buttons.py
+++ Lesson3_Buttons.py
@@ -1,7 +1,13 @@
 from tkinter import *
 
 root = Tk()
+root.geometry('300x300')
 
-myButton = Button(root, text="Click me").pack()
+def myClick():
+    myLabel = Label(root, text="I have been clicked")
+    myLabel.pack()
+
+myButton = Button(root, text="Click me", pady=50, padx=50, command=myClick, fg="blue", bg="lightblue")
+myButton.pack()
 
 root.mainloop()
